2b243eb0046a70fb28fa2260ede8900d0db8781f
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-04-08 12:49:37 +0200

    [TASK] Remove ClassAliasMap.php. Not needed anymore

--- Migrations/Code/ClassAliasMap.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'TYPO3\\JhMagnificpopup\\Controller\\MagnificpopupController' => 'Heilmann\\JhMagnificpopup\\Controller\\MagnificpopupController',
-    'TYPO3\\JhMagnificpopup\\ViewHelpers\\Extension\\LoadedViewHelper' => 'Heilmann\\JhMagnificpopup\\ViewHelpers\\Extension\\LoadedViewHelper',
-    'Heilmann\\JhMagnificpopup\\Controller\\MagnificpopupController' => 'JonathanHeilmann\\JhMagnificpopup\\Controller\\MagnificpopupController',
-    'Heilmann\\JhMagnificpopup\\ViewHelpers\\Extension\\LoadedViewHelper' => 'JonathanHeilmann\\JhMagnificpopup\\ViewHelpers\\Extension\\LoadedViewHelper',
-];
