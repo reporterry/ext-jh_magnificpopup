05846a2fa3a1d8b07ea03a327b2713ab7094b4b2
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-04-13 09:35:01 +0200

    [CLEANUP] Remove old xclassing mechanism

--- Classes/Hooks/UpdateColPosHook.php
+++ Classes/Hooks/UpdateColPosHook.php
@@ -76,7 +76,3 @@ class UpdateColPosHook
         }
     }
 }
-
-if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/jh_magnificpopup/Classes/Hooks/class.tx_jhmagnificpopup_tcemain.php']) {
-    include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/jh_magnificpopup/Classes/Hooks/class.tx_jhmagnificpopup_tcemain.php']);
-}
